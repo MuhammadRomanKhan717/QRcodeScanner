afe8e7889729f68714a23da1568a622ae6b96b5a
MuhammadRomanKhan717 <roman03348669717@gmail.com>
2025-02-11 22:22:54 +0500

    initial

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: ['react-native-reanimated/plugin'],
+  // plugins: [
+  //   ["react-native-auto-route/plugin", { appDirectory: "src" }],
+  //   "nativewind/babel",
+  //   'react-native-reanimated/plugin'
+  // ]
+};
---
+++ metro.config.js
@@ -0,0 +1,13 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+// const config = {};
+const config = {
+  resetCache: true, // this worked for me
+};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
